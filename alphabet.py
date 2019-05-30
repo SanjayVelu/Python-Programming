43a6fb2ff5027d76ad7ff7b9bd54b52f675382ba
SanjayVelu <51081868+SanjayVelu@users.noreply.github.com>
2019-05-30 09:43:50 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+s=['a","b","c","d","e',"f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+n=input()
+n=n.lower()
+if n in s:
+  print("alphabet")
+else:
+  print("not a alphabet")
